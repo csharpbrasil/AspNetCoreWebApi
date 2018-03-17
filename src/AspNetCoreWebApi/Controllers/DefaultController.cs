3f530d5798abb0e363cb538ca10c2a8d67be65a2
Raphael Cardoso <ferronicardoso@outlook.com>
2018-03-17 09:37:01 -0300

    Fonte do artigo Criando Web Api com ASP.NET Core

---
+++ src/AspNetCoreWebApi/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DefaultController : Controller
+    {
+        [HttpGet, Route("get")]
+        public IActionResult Get()
+        {
+            var model_exemplo = new
+            {
+                success = true
+            };
+
+            return Ok(model_exemplo);
+        }
+
+        [HttpPost, Route("post")]
+        public IActionResult Post([FromBody] object valor)
+        {
+            var model_exemplo = new
+            {
+                success = true,
+                value = valor
+            };
+
+            return Ok(model_exemplo);
+        }
+    }
+}
